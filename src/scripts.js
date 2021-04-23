a82124f8979b47c4fcd5cfc44a95ddaff4152114
kendallha <kendallhaw@gmail.com>
2021-04-23 08:31:41 -0700

    Instantiate new class objects using fetch data

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,10 @@
 import './css/styles.scss';
-import './index.js';
 import domUpdates from './dom-updates';
 import Traveler from './traveler';
 import DestinationRepository from './destination-repo';
 import TripRepository from './trip-repo';
+import dayjs from 'dayjs';
+dayjs().format();
 
 let date = dayjs("12/11/2020");
 let traveler;
@@ -11,26 +12,40 @@ let trips;
 let destinations;
 let userIdInput = 1;
 
-window.addEventListener("load", fetchAPIdata);
+let bookingButton = document.querySelector("#bookingButton");
+
+window.addEventListener("load", fetchAPIData);
+bookingButton.addEventListener("click", () => {
+  console.log("click");
+})
 
 function fetchAPIData() {
-  console.log("somethins happenin");
   const travelerPromise = fetch(`http://localhost:3001/api/v1/travelers/${userIdInput}`)
     .then(response => response.json())
     .then(data => data)
   
   const tripsPromise = fetch('http://localhost:3001/api/v1/trips')
-    .then(console.log(response.ok))
     .then(response => response.json())
     .then(data => data)
   
   const destinationsPromise = fetch('http://localhost:3001/api/v1/destinations')
-    .then(console.log(response.ok))
     .then(response => response.json())
     .then(data => data)
 
   Promise.all([travelerPromise, tripsPromise, destinationsPromise])
-    .then(data => console.log(data))
+    .then(data => prepareDOM(data))
     .catch(error => console.log(error))
+}
+
+function prepareDOM([travelerData, tripData, destinationData]) {
+  destinations = new DestinationRepository(destinationData.destinations);
+  console.log(destinations);
+  trips = new TripRepository(tripData.trips, destinations.destinations);
+  getNewTraveler(travelerData, trips);
+}
+
+function getNewTraveler(travelerInfo, tripRepository) {
+  traveler = new Traveler(travelerInfo, tripRepository);
+  //get first name, call DOM function to show it in banner
 }
 
